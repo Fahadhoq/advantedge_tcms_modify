bedc1fec9f3dd1acf66c31d3286d81cb67d6c5c8
Fahadhoq <Fahadulhoq2@gmail.com>
2021-11-30 16:49:40 +0600

    dashboard add

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,13 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use App\Models\User_Type;
+use App\Models\User_Info;
+use App\Models\StudentPayment;
+use App\Models\Income;
+use App\Models\Expense;
+use App\Models\Classes;
 
 class DashboardController extends Controller
 {
     public function index()
-    {                   
-        return view('Dashboard.dashboard' );
+    {   
+        //students
+        $data['User_Infos'] = User_Info::select('user_id' , 'verified_by' , 'user_type_id')->get();
+
+        $data['Students']  = 0;
+
+        foreach ($data['User_Infos'] as $User_Info) {
+            $User_Type = User_Type::select('name')->where('id' , $User_Info->user_type_id)->first();
+
+            if($User_Type->name == 'student' ){
+                $data['Students']++;
+            }
+        }
+
+        //students payment
+        $data['StudentsPayment'] = StudentPayment::get();
+        $data['StudentsPayments'] = 0;
+
+        foreach ($data['StudentsPayment'] as $StudentsPayment) {
+            $data['StudentsPayments'] = $data['StudentsPayments'] + $StudentsPayment->payment_amount;
+        }
+
+        //income
+        $data['Income'] = Income::get();
+        $data['Incomes'] = 0;
+
+        foreach ($data['Income'] as $Income) {
+            $data['Incomes'] = $data['Incomes'] + $Income->amount;
+        }
+
+        //expense
+        $data['Expense'] = Expense::get();
+        $data['Expenses'] = 0;
+
+        foreach ($data['Expense'] as $Expense) {
+            $data['Expenses'] = $data['Expenses'] + $Expense->amount;
+        }
+
+        return view('Dashboard.dashboard' , $data );
     }
 
     public function cancle()
--- resources/views/Dashboard/dashboard.blade.php
+++ resources/views/Dashboard/dashboard.blade.php
@@ -37,32 +37,73 @@
                     <!-- main contaner start -->
                
                     <div class="row">
+
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="card">
+                                <div class="card-heading p-4">
+                                    <div class="mini-stat-icon float-right">
+                                        <i class="mdi mdi-cube-outline bg-primary  text-white"></i>
+                                    </div>
+                                    <div>
+                                        <h5 class="font-16">Total Students</h5>
+                                    </div>
+                                    <h3 class="mt-4">{{$Students}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="card">
+                                <div class="card-heading p-4">
+                                    <div class="mini-stat-icon float-right">
+                                        <i class="mdi mdi-briefcase-check bg-success text-white"></i>
+                                    </div>
+                                    <div>
+                                        <h5 class="font-16">Total Student Payment</h5>
+                                    </div>
+                                    <h3 class="mt-4">{{$StudentsPayments}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="card">
+                                <div class="card-heading p-4">
+                                    <div class="mini-stat-icon float-right">
+                                        <i class="mdi mdi-tag-text-outline bg-warning text-white"></i>
+                                    </div>
+                                    <div>
+                                        <h5 class="font-16">Total Income</h5>
+                                    </div>
+                                    <h3 class="mt-4">{{$Incomes}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="card">
+                                <div class="card-heading p-4">
+                                    <div class="mini-stat-icon float-right">
+                                        <i class="mdi mdi-buffer bg-danger text-white"></i>
+                                    </div>
+                                    <div>
+                                        <h5 class="font-16">Total expenses</h5>
+                                    </div>
+                                    <h3 class="mt-4">{{$Incomes}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
                         <div class="col-xl-8">
                             <div class="card m-b-30">
                                 <div class="card-body">
 
                                     <h4 class="mt-0 header-title mb-4">Area Chart</h4>
 
-                                    <form action="" method="post" >
-                    @csrf
-
-
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-
-
-  <div class="form-group">
-    <label for="password">Password</label>
-    <input type="password" class="form-control" name="password"  placeholder="Password">
-  </div>
-
-
-  <button type="submit" class="btn btn-primary">Submit</button>
-               
-               </form>
+                                    <div id="morris-area-example" class="morris-charts morris-chart-height"></div>
 
                                 </div>
                             </div>
@@ -74,32 +115,12 @@
                                 <div class="card-body">
                                     <h4 class="mt-0 header-title mb-4">Donut Chart</h4>
 
-                                    <form action="" method="post" >
-                    @csrf
-
-
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-
-
-  <div class="form-group">
-    <label for="password">Password</label>
-    <input type="password" class="form-control" name="password"  placeholder="Password">
-  </div>
-
-
-  <button type="submit" class="btn btn-primary">Submit</button>
-               
-               </form>
+                                    <div id="morris-donut-example" class="morris-charts morris-chart-height"></div>
 
                                 </div>
                             </div>
                         </div>
                         <!-- end col -->
-
                     </div>
                     <!-- end row -->
 
